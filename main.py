ad1e89ba50db07c9d055d6fbba2e430c0d42257e
neela <er.neela2012@gmail.com>
2022-07-05 14:26:13 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/detail')
+def detail():
+    return jsonify({'name' : "Neela",
+                    'greet' : "Welcomes you"})
+app.run()
